bd36f8938b2803f13a403e0ba783765db7a0bd8b
Eduardo Ocampo <edd.develit@gmail.com>
2018-03-22 16:58:34 -0600

    Iniciando proyecto

---
+++ src/donas/Main.java
@@ -0,0 +1,26 @@
+package donas;
+
+public class Main {
+	
+	public String epicDay()
+	{
+		
+	boolean donut = true;
+	boolean superSAD = true;
+	String amigos= "Bien culeros pero xidos";
+	String resultado = donut + amigos;
+	String broma = "Succes!";
+
+	if(donut == superSAD){
+	   System.out.print(donut +" estas bien?");
+	}
+	else{
+		System.out.print("Tu padrino guitarrista vende tacos se va a morir :P y tienes una "
+				+ "entrevista de trabajo :3");
+		
+		superSAD = true;
+	}
+
+	return (broma);
+	}
+}
